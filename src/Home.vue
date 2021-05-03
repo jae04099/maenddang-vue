e83f78481a6fbd7eec51a826f9515372dbab67cb
Lena <jae04099@gmail.com>
2021-05-03 18:08:58 +0900

    5번 영상부터 봐야함

---
+++ src/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{homeTitle}}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+       return {
+           homeTitle: "this is home"
+       }
+    },
+}
+</script>
